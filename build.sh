2df938751b7ecd2c955c5099d5569508eeb5bda9
jeff <jeff.zhang223@gmail.com>
2016-10-18 01:42:44 -0700

    ad build script to install pip and django

---
+++ build.sh
@@ -0,0 +1,20 @@
+
+#!/bin/bash
+
+echo "====================================================================="
+echo "make alias python=python3"
+alias python=python3
+
+echo "====================================================================="
+echo "install pip:"
+echo "sudo apt-get install python-pip"
+sudo apt-get install python3-pip
+
+echo "====================================================================="
+echo "install Django:"
+echo "pip install Django"
+pip3 install Django 
+
+
+echo "====================================================================="
+
