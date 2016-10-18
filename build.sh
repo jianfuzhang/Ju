043955351cec27f494af092b8f42c3eed73a2968
jeff <jeff.zhang223@gmail.com>
2016-10-18 02:14:27 -0700

    setup virtualenv and add gitignore

--- build.sh
+++ build.sh
@@ -11,10 +11,19 @@ echo "sudo apt-get install python-pip"
 sudo apt-get install python3-pip
 
 echo "====================================================================="
-echo "install Django:"
-echo "pip install Django"
-pip3 install Django 
-
+echo "install virtualenv"
+echo "sudo pip3 install virtualenv"
+sudo pip3 install virtualenv
 
 echo "====================================================================="
+echo "setup virtual env:"
+echo "virtualenv ENV"
+virtual ENV
+echo "source ENV/bin/activate"
+source ENV/bin/activate
+
+echo "====================================================================="
+echo "install Django:"
+echo "pip3 install Django"
+pip3 install Django 
 
